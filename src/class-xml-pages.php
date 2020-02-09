d8c29b1698e041ce5fd4840d2001181e37d42e24
Ben <bhoogter@ford.com>
2020-02-09 18:05:19 -0500

    Setup testing and dependencies

--- src/class-xml-pages.php
+++ src/class-xml-pages.php
@@ -1,3 +1,5 @@
 <?php
 
+class xml_pages {
 
+}
---
+++ tests/class-xml-pages-test.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+class source_test extends TestCase
+{
+
+	public function testCreatePagesClass(): void
+	{
+		$obj = new xml_pages();
+		$this->assertNotNull($obj);
+    }
+
+}
