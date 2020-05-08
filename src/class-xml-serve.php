1a292f883540f04863e70f36bd2a74b1a2cfb0b1
Ben <bhoogter@yahoo.com>
2020-05-08 19:10:25 -0400

    Remove specialization objects

--- src/class-xml-serve.php
+++ src/class-xml-serve.php
@@ -30,19 +30,19 @@ class xml_serve extends page_handlers
 
         if ($pagesrc != null) {
             if (is_object($pagesrc)) self::$page_source = $pagesrc;
-            else if (file_exists($l = realpath($pagesrc))) self::$page_source = new page_source($l);
-            else if (file_exists($l = realpath(self::$resource_folder . $pagesrc))) self::$page_source = new page_source($l);
-            else if (file_exists($l = realpath(__DIR__ . "/pages.xml"))) self::$page_source = new page_source($l);
+            else if (file_exists($l = realpath($pagesrc))) self::$page_source = new xml_file($l);
+            else if (file_exists($l = realpath(self::$resource_folder . $pagesrc))) self::$page_source = new xml_file($l);
+            else if (file_exists($l = realpath(__DIR__ . "/pages.xml"))) self::$page_source = new xml_file($l);
         }
         if (self::$page_source == null) throw new Exception("Missing argument 3: pages source (filename, xml_file)");
 
         if ($sitesettings != null) {
             if (is_object($sitesettings)) self::$settings = $sitesettings;
-            else if (file_exists($l = realpath($sitesettings))) self::$settings = new site_settings($l);
-            else if (file_exists($l = realpath(self::$resource_folder . $sitesettings))) self::$settings = new site_settings($l);
-            else if (file_exists($l = realpath(__DIR__ . "/site.xml"))) self::$settings = new site_settings($l);
+            else if (file_exists($l = realpath($sitesettings))) self::$settings = new xml_file($l);
+            else if (file_exists($l = realpath(self::$resource_folder . $sitesettings))) self::$settings = new xml_file($l);
+            else if (file_exists($l = realpath(__DIR__ . "/site.xml"))) self::$settings = new xml_file($l);
         }
-        if (self::$settings == null) throw new Exception("Missing argument 4: site settings (filename, site_settings)");
+        if (self::$settings == null) throw new Exception("Missing argument 4: site settings (filename, xml_file)");
 
         if ($extension_source != null) {
             if (is_object($extension_source)) self::$extension_source = $extension_source;
@@ -78,6 +78,112 @@ class xml_serve extends page_handlers
 
     public static function template_name() { return self::$pagedef->get("/pagedef/@template"); }
     public static function generator_name() { return self::$generator; }
+
+    protected static function new_pagepart_xml($element, $pageset, $request)
+    {
+        php_logger::log("CALL (..., $pageset)");
+        $pageset_check = $pageset == '' ? "not(@id)" : "@id='$pageset'";
+        // print xml_file::nodeXml($element);
+        $xml = xml_file::nodeXmlFile($element);
+        $xml->set("/pagedef/@pageset", $pageset);
+        $xml->set("/pagedef/@request", $request);
+        php_logger::dump($xml->saveXML());
+
+        if (($template = self::$page_source->get("/pages/pageset[$pageset_check]/@template")) != '')
+            $xml->set("/pagedef/@template", $template);
+        else if (($template = self::$page_source->get("/pages/pageset[@default]/@template")) != '')
+            $xml->set("/pagedef/@template", $template);
+        return $xml;
+    }
+
+    protected static function page_part_element($index, &$pageset = "", &$http_result = 200)
+    {
+        php_logger::log("CALL ($index, $pageset)");
+        $extension_handler = null;
+
+        $pageset_check = $pageset == '' ? "not(@id)" : "@id='$pageset'";
+        if (substr($index, 0, 1) == '/') $index = substr($index, 1);
+        if (substr($index, -1) == '/') $index = substr($index, 0, strlen($index) - 1);
+        php_logger::log("CALL ($index, $pageset)");
+        if ((self::$page_source->nde("/pages/pageset[$pageset_check]/pagedef[@loc='$index']"))  != null) {
+            $subpageset = self::$page_source->get("/pages/pageset[$pageset_check]/pagedef[@loc='$index']/@pageset");
+            php_logger::log("exact match $index (pageset=$pageset)");
+
+            if ($subpageset != null) {
+                php_logger::log("matched subset default");
+                $pageset = $subpageset;
+                return self::page_part_element("", $subpageset);
+            }
+
+            $match = "/pages/pageset[$pageset_check]/pagedef[@loc='$index']";
+            $result = self::$page_source->nde($match);
+            php_logger::debug("Returning match for: $match");
+            php_logger::dump("\n", xml_file::nodeXml($result));
+            return $result;
+        }
+        $path = $index;
+        $rest = "";
+        while (true) {
+            if ($path == '') break;
+            $x = strrpos($path, '/');
+            if (($x = strrpos($path, '/')) == false) {
+                $rest = "$path/$rest";
+                $path = '';
+            } else {
+                $rest = substr($path, $x + 1) . ($rest == "" ? "" : "/") . $rest;
+                $path = substr($path, 0, $x);
+            }
+            php_logger::log("Searching path tree: path=$path, rest=$rest");
+            php_logger::debug("Searching: /pages/pageset[$pageset_check]/pagedef[@loc='$path']/@pageset");
+
+            $subpageset = self::$page_source->get("/pages/pageset[$pageset_check]/pagedef[@loc='$path']/@pageset");
+            if ($subpageset != null) {
+                php_logger::trace("subpath pageset $pageset");
+                $pageset = $subpageset;
+                $subset_result = self::page_part_element($rest, $pageset);
+                if ($subset_result != null) return $subset_result;
+                php_logger::trace("subpath didn't find.  No 404 handler provided.");
+                break;
+            }
+        }
+
+        $extension_check = self::$page_source->get("/pages/pageset[$pageset_check]/pagedef[@loc='$path']/@extension");
+        if ($extension_check) {
+            php_logger::trace("extension handler on path [$path]: $extension_check");
+            return self::$page_source->nde("/pages/pageset[$pageset_check]/pagedef[@loc='$path']");
+        }
+
+        if ($index == "") {
+            php_logger::debug("CALL  - Checking default on /pages/pageset[$pageset_check]/pagedef[@default]/@loc");
+            if (self::$page_source->get("/pages/pageset[$pageset_check]/pagedef[@default]/@loc") != '') {
+                php_logger::trace("CALL  - default match");
+                return self::$page_source->nde("/pages/pageset[$pageset_check]/pagedef[@default]");
+            }
+        } else {
+            php_logger::trace("Checking 404 on /pages/pageset[$pageset_check]/pagedef[@default404]/@loc");
+            if (self::$page_source->get("/pages/pageset[$pageset_check]/pagedef[@default404]/@loc") != '') {
+                php_logger::trace("404 match");
+                $http_result = 404;
+                return self::$page_source->nde("/pages/pageset[$pageset_check]/pagedef[@default404]");
+            }
+        }
+
+        $http_result = 404;
+        php_logger::log("NO MATCH");
+        return null;
+    }
+
+    public static function page_part($index, &$http_result = 200)
+    {
+        php_logger::log("CALL ($index)");
+        $pageset = "";
+        $element = self::page_part_element($index, $pageset, $http_result);
+        php_logger::debug("pageset=$pageset, http_result=$http_result");
+        if ($element == null) return null;
+        $result = self::new_pagepart_xml($element, $pageset, $index);
+        php_logger::dump($result->saveXML());
+        return $result;
+    }
 
     public static function make_page_xsl()
     {
--- src/stub.php
+++ src/stub.php
@@ -10,8 +10,6 @@ spl_autoload_register(function ($name) {
     $d = (strpos(__FILE__, ".phar") === false ? __DIR__ : "phar://" . __FILE__ . "/src");
     if ($name == "xml_serve") require_once($d . "/class-xml-serve.php");
     if ($name == "page_handlers") require_once($d . "/class-page-handlers.php");
-    if ($name == "page_source") require_once($d . "/class-page-source.php");
-    if ($name == "site_settings") require_once($d . "/class-site-settings.php");
     if ($name == "xml_serve_extensions") require_once($d . "/class-extensions.php");
 });
 
