63515c1a141b5a1d352694ac48d705a96a78d810
Ben <bhoogter@yahoo.com>
2020-05-28 17:10:30 -0400

    Update release script

--- release.sh
+++ release.sh
@@ -4,18 +4,27 @@ set -e -x
 #git pull -r
 
 # Remove '-dev' from the version file to prepare for release.
-sed -e 's/-dev$//' VERSION > VERSION.tmp
+ver="$(cat VERSION | sed -e 's/-dev$//')"
+echo $ver > VERSION.tmp
 mv -f VERSION.tmp VERSION
 
+MAJOR=$(echo "${ver}" | sed -e 's/\.[0-9]*\.[0-9]*$//')
+MINOR=$(echo "${ver}" | sed -e 's/^[0-9]*\.//' | sed -e 's/\.[0-9]*$//')
+PATCH=$(echo "${ver}" | sed -e 's/^[0-9]*.[0-9]*.//')
+
+PATCH=$(( $PATCH + 1 ))
+
+
 # Tag a release
-ver="$(cat VERSION)"
+echo "Releasing version: ${ver}"
 git commit -am "Version $ver"
 git tag "$ver"
 git push origin "$ver"
 
 # Advance to the next patch release, add the '-dev' suffix back on, and commit the result.
-a=( ${ver//./ } ) && ((a[2]++))
-echo "${a[0]}.${a[1]}.${a[2]}-dev" > VERSION
+NEW_VERSION="$MAJOR.$MINOR.$PATCH-dev"
+echo "Setting new version: ${NEW_VERSION}"
+echo "$NEW_VERSION" > VERSION
 git add VERSION
-git commit -m "Back to -dev: $(cat VERSION)"
+git commit -m "Back to -dev: $NEW_VERSION"
 git push origin master
